89e978da2e33d0bb48c103325d58d314b9aa9c23
Deepak Chaudhary <deepak.chaudhary@snva.com>
2024-11-16 12:47:30 +0530

    change in fonts config

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
-import { Poppins } from 'next/font/google';
-
-export const poppins = Poppins({weight: "800"});
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
